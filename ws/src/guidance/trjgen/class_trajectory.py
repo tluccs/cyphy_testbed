4c6f4001020f1d3b8467b9bb0a5de450185c1f45
Luigi Pannocchi <l.pannocchi@gmail.com>
2019-08-15 10:43:52 -0700

    Added computation of reference angular velocity

--- ws/src/guidance/trjgen/class_trajectory.py
+++ ws/src/guidance/trjgen/class_trajectory.py
@@ -45,7 +45,10 @@ class Trajectory:
         # the demanded orientation of the vehicle
 
         R = np.zeros((3,3), dtype=float)
-        if (np.array(derlist) == 2).any():
+        Xb = np.zeros((3), dtype=float)
+        Yb = np.zeros((3), dtype=float)
+        Zb = np.zeros((3), dtype=float)
+        if (np.array(derlist) >= 2).any():
 
             # Assemble the acceleration 3d Vector
             Thrust = np.array((X[2], Y[2], Z[2] + 9.81))
@@ -63,7 +66,23 @@ class Trajectory:
             R[:, 1] = Yb
             R[:, 2] = Zb 
 
-        return (X, Y, Z, W, R)
+        Omega = np.zeros((3), dtype=float)
+        if (np.array(derlist) >= 3).any():
+            u1 = np.linalg.norm(Thrust)
+            a_dot = np.array([X[3], Y[3], Z[3]], dtype=float)
+
+            if (u1 > 0.01):
+                h_omega = (a_dot - np.dot(Zb, a_dot) * Zb) / u1
+            else:
+                h_omega = np.zeros((3), dtype=float)
+
+            # Omega_x
+            Omega[0] = -np.dot(h_omega, Yb)
+            Omega[1] = np.dot(h_omega, Xb)
+            Omega[2] = W[1] * Zb[2]
+
+
+        return (X, Y, Z, W, R, Omega)
 
 
     def writeTofile(self, Dt, filename='./poly.csv'):
@@ -74,5 +93,4 @@ class Trajectory:
         w_coeff = self.py.getCoeffMat();
 
         tj.pp2file(Dt, x_coeff, y_coeff, z_coeff, w_coeff, filename)
-
 
